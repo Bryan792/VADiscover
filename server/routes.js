590bb65f68a4c5f2261110afcee1823e8234320c
Bryan Ching <bryan792@gmail.com>
2016-10-26 07:02:29 -0700

    Add kue with updateUser job

--- server/routes.js
+++ server/routes.js
@@ -3,9 +3,11 @@ const rp = require('request-promise-native');
 const Promise = require('bluebird');
 const _ = require('lodash');
 const cradle = Promise.promisifyAll(require("cradle"));
+const kue = require('kue');
 
 const db = new (cradle.Connection)().database('apijson');
 const responseDb = new (cradle.Connection)().database('responses');
+const queue = kue.createQueue();
 
 function initDb(database) {
   database.exists(createDbIfNeeded);
@@ -25,6 +27,106 @@ function initDb(database) {
 initDb(db);
 initDb(responseDb);
 
+queue.process("updateUser", (job, done) => {
+  updateUser(job.data.title)
+    .then(() => {
+      done();
+      console.log("job done: " + job.data.title);
+    });
+})
+
+async function updateUser(username) {
+  console.log("processing: " + username);
+  await getApiValue("animelist/" + username).then(async function(person) {
+    var counter = {};
+    var actors = {};
+    await Promise.all(person.anime.map(async(anime) => {
+      var addMe = anime.title + "\n";
+      await getApiValue("anime/cast/" + anime.id).then((cast) => {
+        addMe += "actors\n";
+        _(cast.Characters)
+          .filter({
+            "role": "Main"
+          })
+          .map((character) => {
+            return {
+              name: character.name,
+              actors: _(character.actors)
+                .filter({
+                  language: "Japanese"
+                })
+                .map((actor) => {
+                  if (!actors[actor.id]) {
+                    actors[actor.id] = {
+                      actor: actor,
+                      character: [_(character).omit("actors")]
+                    };
+                  } else {
+                    actors[actor.id].character.push(_(character).omit("actors"));
+                  }
+                  let count = counter[actor.id];
+                  if (count == null) {
+                    counter[actor.id] = 1;
+                  } else {
+                    counter[actor.id] = ++count;
+                  }
+                  return actor;
+                })
+            };
+          })
+          .each((character) => {
+            addMe += JSON.stringify(character) + "\n"
+          });
+      }).catch(function(err) {
+        console.log(err);
+      // API call failed... 
+      });
+    }));
+    var sorted = _(counter)
+      .pickBy((count) => {
+        return count > 1;
+      })
+      .toPairs()
+      .sortBy((pair) => {
+        return pair[1];
+      })
+      .reverse()
+      .forEach((value, key, map) => {
+        //console.log(key + " " + value);
+      })
+
+    let response = await Promise.all(
+      _(sorted)
+        .slice(0, 5)
+        .map((value) => {
+          let characters = _(actors[value[0]].character).map((character) => character.value()).uniqBy("id");
+          return getApiValue("people/" + actors[value[0]].actor.id)
+            .then((person) => {
+              return {
+                actor: {
+                  ..._(person).pick(["image_url", "name"]).value(),
+                  id: actors[value[0]].actor.id
+                },
+                character: characters,
+                count: value[1],
+              }
+            })
+        })
+    );
+
+    console.log(username);
+    responseDb.save(username, {
+      response: response
+    }, (err, res) => {
+      console.log(res)
+    });
+
+  }).catch(function(err) {
+    console.log(err);
+  // API call failed... 
+  });
+}
+
 router.get('/mal/:id', async(ctx) => {
   var options = {
     uri: 'http://localhost:9001/2.1/animelist/bryan792',
@@ -39,98 +141,17 @@ router.get('/mal/:id', async(ctx) => {
       console.log("Returning cached response");
       ctx.body = doc.response;
     })
-    .catch(async(err) => {
-      await getApiValue("animelist/" + username).then(async function(person) {
-        var counter = {};
-        var actors = {};
-        ctx.body = "";
-        await Promise.all(person.anime.map(async(anime) => {
-          var addMe = anime.title + "\n";
-          await getApiValue("anime/cast/" + anime.id).then((cast) => {
-            addMe += "actors\n";
-            _(cast.Characters)
-              .filter({
-                "role": "Main"
-              })
-              .map((character) => {
-                return {
-                  name: character.name,
-                  actors: _(character.actors)
-                    .filter({
-                      language: "Japanese"
-                    })
-                    .map((actor) => {
-                      if (!actors[actor.id]) {
-                        actors[actor.id] = {
-                          actor: actor,
-                          character: [_(character).omit("actors")]
-                        };
-                      } else {
-                        actors[actor.id].character.push(_(character).omit("actors"));
-                      }
-                      let count = counter[actor.id];
-                      if (count == null) {
-                        counter[actor.id] = 1;
-                      } else {
-                        counter[actor.id] = ++count;
-                      }
-                      return actor;
-                    })
-                };
-              })
-              .each((character) => {
-                addMe += JSON.stringify(character) + "\n"
-              });
-          //ctx.body += addMe;
-          }).catch(function(err) {
-            console.log(err);
-          // API call failed... 
-          });
-        }));
-        var sorted = _(counter)
-          .pickBy((count) => {
-            return count > 1;
-          })
-          .toPairs()
-          .sortBy((pair) => {
-            return pair[1];
-          })
-          .reverse()
-          .forEach((value, key, map) => {
-            //console.log(key + " " + value);
-          })
+    .catch((err) => {
+      //NEW USER
+      queue.create("updateUser",
+        {
+          title: username
+        }).save();
 
-        let response = await Promise.all(
-          _(sorted)
-            .slice(0, 5)
-            .map((value) => {
-              let characters = _(actors[value[0]].character).map((character) => character.value()).uniqBy("id");
-              return getApiValue("people/" + actors[value[0]].actor.id)
-                .then((person) => {
-                  return {
-                    actor: {
-                      ..._(person).pick(["image_url", "name"]).value(),
-                      id: actors[value[0]].actor.id
-                    },
-                    character: characters,
-                    count: value[1],
-                  }
-                })
-            })
-        );
-
-        console.log(username);
-        responseDb.save(username, {response: response}, (err, res) => {
-          console.log(res)
-        });
-        ctx.body = JSON.stringify(response);
-
-      }).catch(function(err) {
-        console.log(err);
-      // API call failed... 
-      });
+      ctx.body = {
+        last_updated: "never"
+      }
     });
-
 })
 
 function getApiValue(apiPath) {
@@ -140,6 +161,7 @@ function getApiValue(apiPath) {
     return doc;
   }).catch(async(err) => {
     //THIS FEELS REALLY BAD
+    //TODO: add rate limit
     var options = {
       uri: 'http://localhost:9001/2.1/' + apiPath,
       json: true // Automatically parses the JSON string in the response 
